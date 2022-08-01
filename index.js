70ba8fbe8a5168d807b83174f0c9cf8528bbc5a3
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-08-01 11:58:40 -0700

    updated click with other movie info

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ fetch("https://ghibliapi.herokuapp.com/films/")
         span.innerText = movie.title;
         movieBar.append(span);
         span.addEventListener('click', (event) => {
-            
+            const title = document.querySelector('#title');
+            title.innerText = movie.title;
+            const ogTitle = document.querySelector('#ogTitle');
+            ogTitle.innerText = movie.original_title;
+            const image = document.querySelector('#image');
+            image.src = movie.image;
+            image.alt = movie.title;
+            const directorAndProducer = document.querySelector('#directorAndProducer');
+            directorAndProducer.innerText = `Director: ${movie.director}  |  Producer: ${movie.producer}`;
+            const releaseDate = document.querySelector('#releaseDate');
+            releaseDate.innerText = `Release Date: ${movie.release_date}`;
+            const rtScore = document.querySelector('#rtScore');
+            rtScore.innerText = `Rate Score: ${movie.rt_score}`;
+            const description = document.querySelector('#description');
+            description.innerText = `Description: ${movie.description}`;
         })
     })
 });
