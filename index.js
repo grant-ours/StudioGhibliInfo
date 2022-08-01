950caa2ec7aaa40a55f1bd2519eb5bba2693c0c7
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-08-01 10:52:49 -0700

    created movie bar with titles

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 fetch("https://ghibliapi.herokuapp.com/films/")
 .then(response => response.json())
-.then(data => {
-    console.log(data)
-})
+.then(movies => {
+    movies.forEach(movie => {
+        const movieBar = document.querySelector('#movie-bar');
+        const span = document.createElement('span');
+        span.innerText = movie.title;
+        movieBar.append(span);
+    })
+});
