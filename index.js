2c520627429dee025126961764a8c2bd51d95110
Grant Ours <grant.ours55@gmail.com>
2022-08-03 10:27:25 -0600

    trash

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+fetch("https://ghibliapi.herokuapp.com/films/")
+  .then((response) => response.json())
+  .then((movies) => {
+    movies.forEach((movie) => {
+      const movieBar = document.querySelector("#movie-bar");
+      const span = document.createElement("span");
+      span.innerText = movie.title;
+      movieBar.append(span);
+      span.addEventListener("click", (event) => {
+        console.log(event);
+      });
+    });
+  });
